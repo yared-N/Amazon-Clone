56cd1b885743bf351855e98f5c67ea19909af9d4
yared-N <73673404+yared-N@users.noreply.github.com>
2021-04-16 15:44:07 -0400

    updated

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const instance = axios.create({
   // THE API (cloud function) URL
-  baseURL: 'https://us-central1-fir-90b80.cloudfunctions.net/api',
+  // baseURL: 'https://us-central1-fir-90b80.cloudfunctions.net/api',
 });
 
 export default instance;
